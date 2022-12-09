06d9721c4fa17482c2963e9064b7f1ab16c89abd
Fredrick-Omondi <frederickomondi93@gmail.com>
2022-12-08 16:19:25 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0 (Success)
+ * main - main function
+ *
+ * Return: 0 on success
  */
-int main(void)  
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle")
-  return (0); 
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
