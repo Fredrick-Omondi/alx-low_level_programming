4de075a8a0ea6414ca1bdb700866472fe0db6f72
Fredrick-Omondi <frederickomondi93@gmail.com>
2022-12-20 16:04:03 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints a string, followed by a new line, to stdout
+ * @str: input value
+ * Return: print
+ */
+
+void puts2(char *str)
+{
+int i;
+i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
